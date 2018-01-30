df38ee09cc43203c277a2ac63739d214044a1fab
RussW <russ.h.walker@gmail.com>
2018-01-29 19:19:40 -0500

    Initial Commit

---
+++ src/AspNetCorePlayground.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetCorePlayground.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
